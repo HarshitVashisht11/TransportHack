7d4888859ba3860065073a2cd8e7fd574e34b9c7
HarshitVashisht11 <harshitvashisht07@gmail.com>
2023-09-23 21:56:08 +0530

    Upload Succefully

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // Import the Google Maps package
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Road Defect Reporting',
+      debugShowCheckedModeBanner: false,
+      title: 'RoadRaze',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primaryColor: Colors.transparent, // Make the app bar transparent.
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent, // Set app bar background color to transparent.
+          elevation: 0, // Remove shadow from app bar.
+        ),
       ),
-      home: MainPage(),
+      home: const HomePage(),
     );
   }
 }
 
-class MainPage extends StatelessWidget {
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Road Defect Reporting'),
+        title: Text(
+          'RoadRaze',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, // Make the app name bold.
+            color: Colors.black, // Set app name text color to black.
+            fontSize: 28.0, // Increase font size for a larger heading.
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black, // Set hamburger icon color to black.
+            ),
+            onPressed: () {
+              // Open the drawer menu when the hamburger icon is clicked.
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text(
-              'Welcome to the Road Defect Reporting App!',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.transparent, // Make the drawer header transparent.
+              ),
+              child: Text(
+                'Menu Options',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black, // Set text color to black.
+                ),
               ),
             ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Add navigation logic to navigate to report submission screen.
-                // Navigator.push(context, MaterialPageRoute(builder: (context) => ReportSubmissionPage()));
+            ListTile(
+              title: Text('Option 1'),
+              onTap: () {
+                // Handle Option 1.
               },
-              child: Text('Report a Road Defect'),
             ),
+            ListTile(
+              title: Text('Option 2'),
+              onTap: () {
+                // Handle Option 2.
+              },
+            ),
+            // Add more menu options as needed.
           ],
         ),
       ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.7749, -122.4194), // Replace with your desired initial map coordinates.
+              zoom: 12, // Adjust the initial zoom level.
+            ),
+            // Add other Google Map configuration options as needed.
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8), // Add glass effect with opacity.
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  // Add your option icons and labels here.
+                  OptionWidget(icon: Icons.report, label: 'Report Issue'),
+                  OptionWidget(icon: Icons.location_on, label: 'Nearby Incidents'),
+                  OptionWidget(icon: Icons.info, label: 'About Us'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OptionWidget extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const OptionWidget({required this.icon, required this.label, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon, size: 32.0, color: Colors.black), // Icon
+        SizedBox(height: 4.0),
+        Text(
+          label,
+          style: TextStyle(fontSize: 16.0, color: Colors.black),
+        ), // Label
+      ],
     );
   }
 }
---
+++ lib/utils/color.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+const primaryColor = Colors.white;
+const secondaryColor = Colors.yellowAccent;
